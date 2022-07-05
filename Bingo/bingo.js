5d43a9f37605ae87223294376c2b81e9d5331c12
solemonj <solemon.00@gmail.com>
2022-07-05 16:45:58 -0300

    atualização bingo

--- Bingo/bingo.js
+++ Bingo/bingo.js
@@ -18,22 +18,27 @@ function informar(event){
         lista.push(i);
     }
     console.log(lista)
-
 }
+
 
 function sortear(event){
     event.preventDefault()
+    if (lista.length>=1) {
+    // //sorteio a posicao de um numero
+        const posicaosort = Math.floor(Math.random() * lista.length);
+    // //adiciono o numero sorteado a lista dos sorteados
+        listasort.push(lista[posicaosort]);
+    // //removo elemento sorteado da lista geral informando o nmr de posição
+        lista.splice(posicaosort, 1);
+    // //mostro a lista de sorteados no html
+        resultado = document.getElementById('resultado');
+        resultado.innerHTML = listasort; 
+    }else{
+        resultado = document.getElementById('acabou');
+        resultado.innerHTML = '<h2>Todos os números já foram sorteados</h2>'; 
+    }
 
-// //sorteio a posicao de um numero
-    const posicaosort = Math.floor(Math.random() * lista.length);
-// //adiciono o numero sorteado a lista dos sorteados
-    listasort.push(lista[posicaosort]);
-// //removo elemento sorteado da lista geral informando o nmr de posição
-    lista.splice(posicaosort, 1);
-// //mostro a lista de sorteados no html
-    resultado = document.getElementById('resultado');
-    resultado.innerHTML = listasort;
-}
+    }
 
 
 function limpar(event){
@@ -41,5 +46,7 @@ function limpar(event){
     lista = []
     listasort = []
     resultado = document.getElementById('resultado');
-    resultado.innerHTML = listasort;
+    resultado.innerHTML = ''
+    resultado = document.getElementById('acabou');
+    resultado.innerHTML = ''; 
 }
